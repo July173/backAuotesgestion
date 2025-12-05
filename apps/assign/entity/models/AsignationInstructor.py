3a42c2befad970e92f05ee1029084b3f7e704ee0
Reyving <medinafabriany@gmail.com>
2025-12-04 22:08:21 -0500

    Add state_asignation field to AsignationInstructor model

--- apps/assign/entity/models/AsignationInstructor.py
+++ apps/assign/entity/models/AsignationInstructor.py
@@ -9,6 +9,7 @@ class AsignationInstructor(models.Model):
     instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE, related_name="asignations")
     request_asignation = models.OneToOneField('assign.RequestAsignation', on_delete=models.CASCADE, related_name="asignation_instructor")
     date_asignation = models.DateField(auto_now_add=True)
+    state_asignation = models.CharField(max_length=50)
     active = models.BooleanField(default=True)
     delete_at = models.DateTimeField(null=True, blank=True)
 
